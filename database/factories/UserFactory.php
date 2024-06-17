ecc9bd2b95b129dd9e0a4bda09bda609c69d3eab
Alexandre Osovski <alexandreosovski1@gmail.com>
2024-06-17 19:31:31 -0300

    fix: adjustes in factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => "xandinhouiuiui",
             'email' => fake()->unique()->safeEmail(),
             'user' => "user.toolzz.teste",
             'email_verified_at' => now(),
